e1cfd8bc27d7d8256a04a81a0423ade7b15beea2
lymiao888 <3041105277@qq.com>
2024-04-04 14:39:37 +0800

    feat: run dp A100 and H800

--- run_client.sh
+++ run_client.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+host=`hostname -s`
+if [ "$host" == "hepnode2" ] || [ "$host" == "hepnode3" ] || [ "$host" == "hepnode4" ]; then
 singularity exec --bind /data:/mnt --writable -e ~/container \
-    bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir ~/trt-llm-engines/bs256-h800-awq-int4-int8 --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 20"
+    bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir ~/trt-llm-engines/bs64-A100-awq-int4-int8 --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 300"
+else
+singularity exec --bind /data:/mnt --writable -e ~/container \
+    bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir ~/trt-llm-engines/bs256-h800-awq_int4_fp8 --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 300"
+fi
