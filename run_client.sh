372204f31d42706171eee6ed11098bf112efc554
lymiao888 <3041105277@qq.com>
2024-04-04 23:57:32 +0800

    feat: slurm update

--- run_client.sh
+++ run_client.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 host=`hostname -s`
-if [ "$host" == "hepnode2" ] || [ "$host" == "hepnode3" ] || [ "$host" == "hepnode4" ]; then
+if [ "$host" == "hepnode3" ] || [ "$host" == "hepnode4" ]; then
+if [ "$SLURM_LOCALID" == "0" ];then
+    singularity exec --bind /data:/mnt --writable -e ~/container \
+        bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir $H800_engine_dir --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 300"
+else
+    singularity exec --bind /data:/mnt --writable -e ~/container \
+        bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir $A100_engine_dir --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 300"
+fi
+elif [ "$host" == "hepnode2" ]; then
 singularity exec --bind /data:/mnt --writable -e ~/container \
-    bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir $A100_engine_dir --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 300"
+    bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir $A100_engine_dir --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 300"    
 else
 singularity exec --bind /data:/mnt --writable -e ~/container \
     bash -c "CUDA_VISIBLE_DEVICES=$SLURM_LOCALID ./build/client --engine_dir $H800_engine_dir --eos_id 100007 --pad_id 0 --rpc_address $headaddr --rpc_port $port --rpc_seqs_threshold 300"
