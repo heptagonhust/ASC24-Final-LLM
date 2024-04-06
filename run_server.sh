962ffe15231126895d020ca94f7f0d251ce5fd22
Eigensystem <eigensystem1318@gmail.com>
2024-04-06 13:16:58 +0800

    fix: fit to the containers running in new driver

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 host=`hostname -s`
+echo "server host: $host"
 if [ "$host" == "$headnode" ] && [ $SLURM_LOCALID == "0" ]; then
     ./build/server --dataset ./data.json --server_addr $headaddr --server_port $port
 fi
