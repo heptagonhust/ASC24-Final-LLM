ded6749f43819732948a2e790fda334466a567b7
MiRaCLeXeoN <zzp905020708@gmail.com>
2024-04-11 08:54:00 +0800

    fix: mmlu csv read

--- src/mmlu/mmlu.cpp
+++ src/mmlu/mmlu.cpp
@@ -3,6 +3,7 @@
 #include "tokenizers/tokenizers_cpp.h"
 #include <cstdint>
 #include <cstdio>
+#include <iostream>
 #include <memory>
 #include <sstream>
 #include <stdexcept>
@@ -10,88 +11,85 @@
 #include <unordered_map>
 #include <vector>
 
-
-inline static std::string LoadBytesFromFile(const std::filesystem::path& path) {
-    std::ifstream fs(path, std::ios::in | std::ios::binary);
-    if (fs.fail()) {
-      std::cerr << "Cannot open tokenzier: " << path.string() << std::endl;
-      exit(1);
-    }
-    std::string data;
-    fs.seekg(0, std::ios::end);
-    size_t size = static_cast<size_t>(fs.tellg());
-    fs.seekg(0, std::ios::beg);
-    data.resize(size);
-    fs.read(data.data(), size);
-    return data;
+inline static std::string LoadBytesFromFile(const std::filesystem::path &path) {
+  std::ifstream fs(path, std::ios::in | std::ios::binary);
+  if (fs.fail()) {
+    std::cerr << "Cannot open tokenzier: " << path.string() << std::endl;
+    exit(1);
+  }
+  std::string data;
+  fs.seekg(0, std::ios::end);
+  size_t size = static_cast<size_t>(fs.tellg());
+  fs.seekg(0, std::ios::beg);
+  data.resize(size);
+  fs.read(data.data(), size);
+  return data;
 }
 
-void convertSeqAndQuestionToStr(
-    std::string& ret,
-    const Sequence& seq,
-    const std::string& question
-) {
-    ret = "";
-    ret += "Question:\n";
-    ret += question;
-    ret += "\n\nChoices:";
-    ret += "\nA: "; ret += seq.optionA.value();
-    ret += "\nB: "; ret += seq.optionB.value();
-    ret += "\nC: "; ret += seq.optionC.value();
-    ret += "\nD: "; ret += seq.optionD.value();
-    ret += "\n\nAnswer: "; ret.push_back(seq.answer.value());
-    ret += "\n\n\n";
+void convertSeqAndQuestionToStr(std::string &ret, const Sequence &seq,
+                                const std::string &question) {
+  ret = "";
+  ret += "Question:\n";
+  ret += question;
+  ret += "\n\nChoices:";
+  ret += "\nA: ";
+  ret += seq.optionA.value();
+  ret += "\nB: ";
+  ret += seq.optionB.value();
+  ret += "\nC: ";
+  ret += seq.optionC.value();
+  ret += "\nD: ";
+  ret += seq.optionD.value();
+  ret += "\n\nAnswer: ";
+  ret.push_back(seq.answer.value());
+  ret += "\n\n\n";
 }
-
 
 /**
  * @brief Generate heading prompts
- * 
- * @param prompt 
- * @param seqs 
- * @param questions 
- * @param categoryName 
+ *
+ * @param prompt
+ * @param seqs
+ * @param questions
+ * @param categoryName
  */
-void generateHeadingPrompt(
-    std::string& ret,
-    const std::vector<Sequence>& seqs,
-    const std::vector<std::string>& questions,
-    const std::string& categoryName
-) {
-    int size = seqs.size();
-    ret = "The following are multiple choice questions (with answers) about ";
-    ret += categoryName;
-    ret += ".\n\n";
+void generateHeadingPrompt(std::string &ret, const std::vector<Sequence> &seqs,
+                           const std::vector<std::string> &questions,
+                           const std::string &categoryName) {
+  int size = seqs.size();
+  ret = "The following are multiple choice questions (with answers) about ";
+  ret += categoryName;
+  ret += ".\n\n";
 
-    // Put example questions
-    for (int i = 0; i < size; i++) {
-        std::string localPrompt;
-        convertSeqAndQuestionToStr(localPrompt, seqs[i], questions[i]);
-        ret += localPrompt;
-    }
+  // Put example questions
+  for (int i = 0; i < size; i++) {
+    std::string localPrompt;
+    convertSeqAndQuestionToStr(localPrompt, seqs[i], questions[i]);
+    ret += localPrompt;
+  }
 }
 
 void getFormattedQuestionIntoSeq(
-    Sequence& seq,
-    const std::string& question,
-    const std::string& headingPrompt,
-    std::shared_ptr<tokenizers::Tokenizer> tknizer
-) {
-    std::string prompt = headingPrompt;
+    Sequence &seq, const std::string &question,
+    const std::string &headingPrompt,
+    std::shared_ptr<tokenizers::Tokenizer> tknizer) {
+  std::string prompt = headingPrompt;
 
-    std::string realQuestion;
-    convertSeqAndQuestionToStr(realQuestion, seq, question);
-    // Get rid of Answer
-    realQuestion.erase(realQuestion.size() - 4);
+  std::string realQuestion;
+  convertSeqAndQuestionToStr(realQuestion, seq, question);
+  // Get rid of Answer
+  realQuestion.erase(realQuestion.size() - 4);
 
-    prompt += realQuestion;
+  prompt += realQuestion;
 
-    seq.inputIds = tknizer->Encode(prompt);
+//   std::cout << "prompt=\n" << prompt << std::endl;
+
+  seq.inputIds = tknizer->Encode(prompt);
 }
 
 std::string MMLU::format_question(const std::string &question,
-                            const std::vector<std::string> &options,
-                            std::string answer, bool ex = false) {
+                                  const std::vector<std::string> &options,
+                                  std::string answer, bool ex = false) {
   std::string clabels = "ABCD";
   std::string text = "问题:\n";
   text += question + "\n\n选项:\n";
@@ -115,110 +113,124 @@ std::string MMLU::format_question(const std::string &question,
   return text;
 }
 
-
 /**
  * @brief Read one question in to seq. This function will not
           modify seq'inputIds. Question will be return in &string.
- * 
- * @param ss 
- * @param seq 
- * @param question 
- * @return true 
- * @return false 
+ *
+ * @param ss
+ * @param seq
+ * @param question
+ * @return true
+ * @return false
  */
-bool readOneQuestion(
-    std::stringstream& ss,
-    Sequence& seq,
-    std::string& question
-) {
-    // read 
-    const int segCount = 6;
-    for (int i = 0; i < segCount; i++) {
-        std::string seg;
-        char tmpChar = 0;
-        // We have a quoted segmentation
-        if (ss.peek() == '"') {
-            while (!ss.eof() && ss.get(tmpChar)) {
-                // closing quote is found
-                if (tmpChar == '"') {
-                    seg.erase(0, 1);
-                    break;
-                }
-                seg.push_back(tmpChar);
-            }
-        } else {
-            getline(ss, seg, ',');
+bool readOneQuestion(std::stringstream &ss, Sequence &seq,
+                     std::string &question) {
+  // read
+  const int segCount = 6;
+  for (int i = 0; i < segCount; i++) {
+    std::string seg;
+    char tmpChar = 0;
+    // We have a quoted segmentation
+    if (ss.peek() == '"') {
+        ss.get(tmpChar);  // skip starting quote
+      while (!ss.eof()) {
+        ss.get(tmpChar);
+        // closing quote is found
+        if (tmpChar == '"') {
+          ss.get(tmpChar);  // Get rid of tailing `,`
+          break;
         }
-        switch (i) {
-        case 0: question = seg; break;
-        case 1: seq.optionA = seg; break;
-        case 2: seq.optionB = seg; break;
-        case 3: seq.optionC = seg; break;
-        case 4: seq.optionD = seg; break;
-        case 5: seq.answer = seg[0]; break;
-        default: throw "Invalid segmentation is detected!";
-        }
+        seg.push_back(tmpChar);
+      }
+    } else {
+      getline(ss, seg, ',');
     }
-    return true;
+    switch (i) {
+    case 0:
+      question = seg;
+      break;
+    case 1:
+      seq.optionA = seg;
+      break;
+    case 2:
+      seq.optionB = seg;
+      break;
+    case 3:
+      seq.optionC = seg;
+      break;
+    case 4:
+      seq.optionD = seg;
+      break;
+    case 5:
+      seq.answer = seg[0];
+      break;
+    default:
+      throw "Invalid segmentation is detected!";
+    }
+  }
+  return true;
 }
 
 // csv format:
 // ,Question,A,B,C,D,Answer
 // 0,下列作物的果实为荚果的是,花生,向日葵,油菜,荞麦,A
-SeqQ MMLU::readDatasetFromCSV(
-    const std::filesystem::path& datasetPath, 
-    const std::filesystem::path& tokenizerPath
-){   
-    const int NUM_EXAMPLES = 3;
-    std::string blob = LoadBytesFromFile(tokenizerPath);
-    auto tok = tokenizers::Tokenizer::FromBlobJSON(blob);
-    std::shared_ptr<tokenizers::Tokenizer> tknizer = std::move(tok);
- 
-    SeqQ seqs;
-    std::ifstream file(datasetPath);
-    std::string fileNameWithoutExtension = datasetPath.stem();
-    std::string line;
-    std::vector<Sequence> example_seqs;
-    std::vector<std::string> example_questions;
-    // skip first line
+SeqQ MMLU::readDatasetFromCSV(const std::filesystem::path &datasetPath,
+                              const std::filesystem::path &tokenizerPath) {
+  const int NUM_EXAMPLES = 3;
+  std::string blob = LoadBytesFromFile(tokenizerPath);
+  auto tok = tokenizers::Tokenizer::FromBlobJSON(blob);
+  std::shared_ptr<tokenizers::Tokenizer> tknizer = std::move(tok);
+
+  SeqQ seqs;
+  std::ifstream file(datasetPath);
+  std::string fileNameWithoutExtension = datasetPath.stem();
+  std::string line;
+  std::vector<Sequence> example_seqs;
+  std::vector<std::string> example_questions;
+  // skip first line
+  getline(file, line);
+
+  // Get example problems
+  for (int i = 0; i < NUM_EXAMPLES; i++) {
     getline(file, line);
-    
-    // Get example problems
-    for(int i = 0; i < NUM_EXAMPLES; i++){
-        getline(file, line);
-        std::stringstream ss(line);
+    std::stringstream ss(line);
+    // std::cout << "line=" << line << std::endl;
 
-        Sequence seq;
-        std::string question;
+    Sequence seq;
+    std::string question;
 
-        readOneQuestion(ss, seq, question);
+    readOneQuestion(ss, seq, question);
 
-        example_seqs.push_back(seq);
-        example_questions.push_back(question);
-    }
+    example_seqs.push_back(seq);
+    example_questions.push_back(question);
+  }
 
-    // prepare heading prompt
-    std::string headingPrompt;
-    generateHeadingPrompt(headingPrompt, example_seqs, example_questions, 
-                          fileNameWithoutExtension.substr(0, fileNameWithoutExtension.size() - 5));
-    // get each problem
-    while (getline(file, line)) {
-        std::stringstream ss(line);
-        std::string temp;
+  // prepare heading prompt
+  std::string headingPrompt;
+  generateHeadingPrompt(
+      headingPrompt, example_seqs, example_questions,
+      fileNameWithoutExtension.substr(0, fileNameWithoutExtension.size() - 5));
 
-        Sequence seq;
-        std::string question;
-        readOneQuestion(ss, seq, question);
+  // get each problem
+  while (getline(file, line)) {
+    std::stringstream ss(line);
+    std::string temp;
 
-        getFormattedQuestionIntoSeq(seq, question, headingPrompt, tknizer);
-        
-        seqs.push(seq);
-    }
-    return seqs;
+    Sequence seq;
+    std::string question;
+    readOneQuestion(ss, seq, question);
+
+    getFormattedQuestionIntoSeq(seq, question, headingPrompt, tknizer);
+
+
+    seqs.push(seq);
+  }
+  return seqs;
 }
 
-SeqQ MMLU::readDatasetFromCSVfolder(const std::filesystem::path &folderPath,
-                              const std::filesystem::path &tokenizerPath) {
+SeqQ MMLU::readDatasetFromCSVfolder(
+    const std::filesystem::path &folderPath,
+    const std::filesystem::path &tokenizerPath) {
 
   auto blob = LoadBytesFromFile(tokenizerPath);
   auto tok = tokenizers::Tokenizer::FromBlobJSON(blob);
@@ -299,12 +311,15 @@ SeqQ MMLU::readDatasetFromCSVfolder(const std::filesystem::path &folderPath,
   return seqs;
 }
 
-float MMLU::output_acc(SeqV V_input, std::variant<ResultV, std::vector<std::vector<float>>> Vector_output,
-                 const std::filesystem::path &tokenizerPath) {
+float MMLU::output_acc(
+    SeqV V_input,
+    std::variant<ResultV, std::vector<std::vector<float>>> Vector_output,
+    const std::filesystem::path &tokenizerPath) {
   auto blob = LoadBytesFromFile(tokenizerPath);
   auto tok = tokenizers::Tokenizer::FromBlobJSON(blob);
   std::vector<std::vector<float>> logits_output;
-  assert(std::holds_alternative<std::vector<std::vector<float>>>(Vector_output));
+  assert(
+      std::holds_alternative<std::vector<std::vector<float>>>(Vector_output));
   logits_output = std::get<std::vector<std::vector<float>>>(Vector_output);
 
   int num = logits_output.size();
